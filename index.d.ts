12141b71a5bde3b07bfb6d9c2cbd165ccfde81fe
indooorsman <indooorsman@gmail.com>
2024-09-27 17:16:12 +0800

    v8.7.0

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,25 @@
 import { Linter, ESLint } from 'eslint';
 
-export declare const parser: Linter.FlatConfigParserModule;
+export declare const parser: Linter.Parser;
 export declare const plugin: ESLint.Plugin;
 export declare const configs: {
-  all: Linter.FlatConfig[];
-  base: Linter.FlatConfig;
-  disableTypeChecked: Linter.FlatConfig;
-  eslintRecommended: Linter.FlatConfig;
-  recommended: Linter.FlatConfig[];
-  recommendedTypeChecked: Linter.FlatConfig[];
-  recommendedTypeCheckedOnly: Linter.FlatConfig[];
-  strict: Linter.FlatConfig[];
-  strictTypeChecked: Linter.FlatConfig[];
-  strictTypeCheckedOnly: Linter.FlatConfig[];
-  stylistic: Linter.FlatConfig[];
-  stylisticTypeChecked: Linter.FlatConfig[];
-  stylisticTypeCheckedOnly: Linter.FlatConfig[];
+  all: Linter.Config[];
+  base: Linter.Config;
+  disableTypeChecked: Linter.Config;
+  eslintRecommended: Linter.Config;
+  recommended: Linter.Config[];
+  recommendedTypeChecked: Linter.Config[];
+  recommendedTypeCheckedOnly: Linter.Config[];
+  strict: Linter.Config[];
+  strictTypeChecked: Linter.Config[];
+  strictTypeCheckedOnly: Linter.Config[];
+  stylistic: Linter.Config[];
+  stylisticTypeChecked: Linter.Config[];
+  stylisticTypeCheckedOnly: Linter.Config[];
 };
 
-interface ConfigWithExtends extends Linter.FlatConfig {
-  extends?: Linter.FlatConfig[];
+interface ConfigWithExtends extends Linter.Config {
+  extends?: Linter.Config[];
 }
 
-export declare function config(...configs: ConfigWithExtends[]): Linter.FlatConfig[];
+export declare function config(...configs: ConfigWithExtends[]): Linter.Config[];
